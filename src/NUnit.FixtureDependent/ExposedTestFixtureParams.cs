0baf3084e2372728db15bc3f94faa6efe87c38a3
An Ionescu <anlogcode@gmail.com>
2019-04-22 18:18:39 +0300

    Adding class that allows users to set text fixture parameters.

---
+++ src/NUnit.FixtureDependent/ExposedTestFixtureParams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework.Interfaces;
+
+namespace NUnit.FixtureDependent
+{
+    /// <summary>
+    /// Has the sole purpose of easily allowing us to create an instance of
+    /// <see cref="ITestFixtureData"/> and set its properties.
+    /// </summary>
+    public class ExposedTestFixtureParams : ITestFixtureData
+    {
+        public Type[] TypeArgs { get; set; }
+
+        public string TestName { get; set; }
+
+        public RunState RunState { get; set; }
+
+        public object[] Arguments { get; set; }
+
+        public IPropertyBag Properties { get; set; }
+    }
+}
