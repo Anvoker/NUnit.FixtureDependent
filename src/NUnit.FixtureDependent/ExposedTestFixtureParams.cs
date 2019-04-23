5872ea97b8a1e7af042176887c5f3ca4631dde82
An Ionescu <anlogcode@gmail.com>
2019-04-23 16:57:23 +0300

    Adding fluent type argument setting method.

--- src/NUnit.FixtureDependent/ExposedTestFixtureParams.cs
+++ src/NUnit.FixtureDependent/ExposedTestFixtureParams.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
 
 namespace NUnit.FixtureDependent
 {
     /// <summary>
     /// Has the sole purpose of easily allowing us to create an instance of
-    /// <see cref="ITestFixtureData"/> and set its properties.
+    /// <see cref="ITestFixtureData"/> where we can manually set the type
+    /// arguments.
     /// </summary>
     public class ExposedTestFixtureParams : ITestFixtureData
     {
@@ -18,5 +20,25 @@ namespace NUnit.FixtureDependent
         public object[] Arguments { get; set; }
 
         public IPropertyBag Properties { get; set; }
+
+        public ExposedTestFixtureParams(object data)
+        {
+            Arguments = new object[] { data };
+            Properties = new PropertyBag();
+            RunState = RunState.Runnable;
+        }
+
+        public ExposedTestFixtureParams(params object[] data)
+        {
+            Arguments = data;
+            Properties = new PropertyBag();
+            RunState = RunState.Runnable;
+        }
+
+        public ExposedTestFixtureParams SetTypeArgs(params Type[] types)
+        {
+            TypeArgs = types;
+            return this;
+        }
     }
 }
